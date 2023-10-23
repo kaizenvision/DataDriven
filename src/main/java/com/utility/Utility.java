15510018776807cedf6cb58d894bb14e37132449
shreyasjoshi <kit.shreyasjoshi@gmail.com>
2023-10-23 19:22:30 +0530

    DGTL-1:- dataprovider code added

--- src/main/java/com/utility/Utility.java
+++ src/main/java/com/utility/Utility.java
@@ -1,7 +1,6 @@
 package com.utility;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.HashMap;
@@ -36,7 +35,7 @@ public class Utility extends BaseClass {
 			return sh.getRow(rowNum).getCell(cellNum).getNumericCellValue();
 	}
 	
-	public Object[][] getAllExcelData(Sheet sh) {
+	public HashMap<String, String> getAllExcelData(Sheet sh) {
 		int rowCount = sh.getLastRowNum();
 		HashMap<String, String> hm = new HashMap<>();
 		Object[][] dataprovider =  new Object[rowCount][2];
@@ -51,15 +50,50 @@ public class Utility extends BaseClass {
 			
 			
 		}
-		int i = 0;
-		for(Map.Entry<String, String> map : hm.entrySet() ) {
-			while(rowCount>0) {
-				dataprovider[i][0] = map.getKey();
-				dataprovider[i][1] = map.getValue();
+		return hm;
+	}
+
+	public Object[][] getData(Sheet sh) {
+
+		int rowCount = sh.getLastRowNum();
+
+		Map<String, Object> finalData = new HashMap<>();
+
+		Object[][] excelData = new Object[rowCount][1];
+
+
+		for(int i=0; i<rowCount; i++) {
+
+			Map<String, Object> data = new HashMap<>();
+
+			int colNum = sh.getRow(i).getLastCellNum();
+
+			for(int j=0; j<colNum; j++) {
+
+				if(sh.getRow(i).getCell(j).getCellType().toString().equalsIgnoreCase("string")) {
+					data.put(sh.getRow(0).getCell(j).getStringCellValue(),
+							sh.getRow(i+1).getCell(j).getStringCellValue());
+				}
+				else if(sh.getRow(i).getCell(j).getCellType().toString().equalsIgnoreCase("numeric")){
+					data.put(sh.getRow(0).getCell(j).getStringCellValue(),
+							sh.getRow(i+1).getCell(j).getNumericCellValue());
+				}
+				else {
+					System.out.println("cell type not match..");
+				}
+
 			}
+
+			excelData[i][0] = data;
+
+
+
+			data.forEach(finalData::put);
+
+
 		}
-		
-		return dataprovider;
+
+		return excelData;
 	}
 
 }
